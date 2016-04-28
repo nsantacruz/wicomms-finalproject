75540c9a72bae5c5a5d1260be4b88453a5449bf3
Noah Santacruz <noahssantacruz@gmail.com>
2016-04-27 23:00:51 -0400

    rayleigh channel

--- testBench.m
+++ testBench.m
@@ -4,14 +4,17 @@ numbits = 1024;
 M = 2;
 k = log2(M);
 snr = 20;
-channel = [1,0.2,0.7];
+symbolPeriod = 1E-4;
+coherenceTime = 10; %should be much greater than symbol period for slow fading
+dopplerShift = 1/coherenceTime; 
+channel = rayleighchan(symbolPeriod,dopplerShift);
+channel.StorePathGains = true;
 
 
 bits = randi([0,1],1,numbits);
 msg = bi2de(reshape(bits,k,size(bits,2)/k).','left-msb')';
 x = qammod(msg,M);
-xc = conv(channel,x);
-xc = xc(1:length(x));
+xc = filter(channel,x);
 xcn = awgn(xc,snr,'measured');
 yn = qamdemod(xcn,M,0,'gray');
 yn = de2bi(yn,'left-msb')';
