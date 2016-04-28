56cdb6d5e700610512c631ab0adff41c7b486afe
Noah Santacruz <noahssantacruz@gmail.com>
2016-04-27 22:46:09 -0400

    back to the basics

--- testBench.m
+++ testBench.m
@@ -1,3 +1,23 @@
 clc, clear, close all;
 
-disp('Final Project!');
+numbits = 1024;
+M = 2;
+k = log2(M);
+snr = 20;
+channel = [1,0.2,0.7];
+
+
+bits = randi([0,1],1,numbits);
+msg = bi2de(reshape(bits,k,size(bits,2)/k).','left-msb')';
+x = qammod(msg,M);
+xc = conv(channel,x);
+xc = xc(1:length(x));
+xcn = awgn(xc,snr,'measured');
+yn = qamdemod(xcn,M,0,'gray');
+yn = de2bi(yn,'left-msb')';
+
+[numerr,ratioerr] = biterr(bits,yn)
+
+
+
+
